7e5b73170e71c541508ce42f0e332b435f7f1281
Naomi Miles <naomi.miles@uhi.ac.uk>
2011-06-14 11:31:24 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+gem 'rake', '~> 0.8.7'
+
+group :development do
+  gem 'rspec-rails', '2.0.0.beta.18'
+  gem 'annotate-models'
+  gem 'faker', '0.3.1'
+end
+
+group :test do
+  gem 'rspec-rails', '2.0.0.beta.18'
+  gem 'webrat'
+  gem 'spork', '0.8.4'
+  gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clare::Application.config.secret_token = 'eb127028511c4d24ceeb2e99d3f335f4d1b71992cd14ec42b0db744691eedf3f83161d3445660e3c3544e3d4f61afc4be566692be6ad0efa81b21247daefb6e3'
